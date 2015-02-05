a70fa45cf4ab7964d61a44163fedf6e3228ce4af
el-antiguo <codomodragon2@gmail.com>
2015-02-05 10:12:41 -0430

    Update basic_func.lib.php

--- sistema/lib/basic_func.lib.php
+++ sistema/lib/basic_func.lib.php
@@ -1,11 +1,19 @@
 <?php
 class basic_func 
 {
+  private $cadena;
+  private $cadena2;
+  private $resultado;
+
   function Limpiar($cadena)
   {
     return (preg_replace('/[^A-Za-z0-9_-ñÑ]/', '', $cadena));
   }
 
+ /* 
+  * obsoleta
+  * 
+  */
   function FormaUrl($cadena2,$cadena){
     if($this->cadena2 =='entrante'){
       return (str_replace('_', ' ', selft::Limpiar($this->cadena)));
@@ -15,6 +23,36 @@ class basic_func
       $cadena= strtr($this->cadena, $con_acento, $sin_acento);
       return (str_replace(' ', '_', $cadena));
     }
+  }
+
+ /*
+  * captura y limpia variaiables tipo get y post
+  *
+  * ejemplo de uso:
+  *   $get = $this->Capturar('post','usuario');
+  * lo cual devuelve
+  *   "usuario"
+  *
+  * @access public
+  *
+  */
+  function Capturar($cadena,$cadena2){
+    if ($cadena=='get'){
+      if (isset($_GET[$cadena2])){
+        $resultado = self::limpiar($_GET[$cadena2]);
+      }else{
+        $resultado = 'get no encontrado';
+      }
+    }else if($cadena=='post'){
+      if (isset($_POST[$cadena2])){
+        $resultado = self::limpiar($_POST[$cadena2]);
+      }else{
+        $resultado = 'post no encontrado';
+      }
+    }else{
+      $resultado = 'Error condicion 1 no valido';
+    }
+    return $resultado;
   }
 }
  ?>
