a1b3e8c15713f5b235495dee11e6017c08bb1dd5
el-antiguo <codomodragon2@gmail.com>
2015-02-04 19:36:42 -0430

    Update basic_func.lib.php
    se agregaron 2 funciones básicas
    
    1 Limpiar(string) limpia las cadenas recibidas de cualquier código malicioso.
    
    2 FormaUrl(constante,string) limpia el string recibido,
    pose 2 constantes  entrante | saliente 
    entrante hace uso de la función limpiar para limpiar el string además de eliminar los "_" y remplazarlos por " "
    saliente elimina los acentos y remplaza los espacios del string por "_"

--- sistema/lib/basic_func.lib.php
+++ sistema/lib/basic_func.lib.php
@@ -1,7 +1,23 @@
 <?php
 class basic_func 
 {
-  function limpiar(){
+  private $cadena
+  private $cadena2
+
+  function Limpiar($this->cadena)
+  {
+    return (preg_replace('/[^A-Za-z0-9_-ñÑ]/', '', $this->cadena));
+  }
+
+  function FormaUrl($this->cadena2,$this->cadena){
+    if($this->cadena2 =='entrante'){
+      return (str_replace('_', ' ', limpiar($this->cadena)));
+    }else if($this->cadena2 =='saliente'){
+      $con_acento = "ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ";
+      $sin_acento = "AAAAAAaaaaaaOOOOOOooooooEEEEeeeeCcIIIIiiiiUUUUuuuuyNn";
+      $cadena= strtr($this->cadena, $con_acento, $sin_acento);
+      return (str_replace(' ', '_', $cadena));
+    }
   }
 }
  ?>
